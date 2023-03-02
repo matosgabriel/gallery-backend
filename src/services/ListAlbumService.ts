194817fc9f4d659f6c081860dea2bad759eaa739
Gabriel Matos <gabriel.ifsal@gmail.com>
2023-03-02 17:56:27 -0300

    Include AlbumList relation field to Album listing

--- src/services/ListAlbumService.ts
+++ src/services/ListAlbumService.ts
@@ -3,7 +3,9 @@ import { prismaClient } from '../prisma';
 
 class ListAlbumService {
   public async execute(): Promise<Album[]> {
-    const albumList = await prismaClient.album.findMany();
+    const albumList = await prismaClient.album.findMany({
+      include: { AlbumFile: true }
+    });
 
     return albumList;
   }
